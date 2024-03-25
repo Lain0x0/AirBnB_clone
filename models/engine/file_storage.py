5ac4266e663e771f4ec2cf0e3e61e788ea399cb4
Lain0x0 <lainyamato03@gmail.com>
2024-03-25 01:54:58 +0000

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-Serializes instances to a JSON file and
-deserializes JSON file to instances.
+Importing Modules
 """
 
 import json
@@ -15,20 +14,19 @@ from models.amenity import Amenity
 from models.review import Review
 
 class_dict = {
-        "BaseModel": BaseModel,
-        "User": User,
-        "Place": Place,
-        "Amenity": Amenity,
-        "City": City,
-        "Review": Review,
-        "State": State
-        }
+    "BaseModel": BaseModel,
+    "User": User,
+    "Place": Place,
+    "Amenity": Amenity,
+    "City": City,
+    "Review": Review,
+    "State": State
+}
 # Filestorage == type(self)
 
 
 class FileStorage:
-    """The file storage engine class, that is;
-    A class that serialize and deserialize instances to a JSON file
+    """The file storage engine class
     """
     __file_path = "file.json"
     __objects = {}
@@ -44,31 +42,25 @@ class FileStorage:
             return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         type(self).__objects[key] = obj
-        # OR
-        # type(self).__objects[obj.id] = obj
 
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
         new_dict = []
         for obj in type(self).__objects.values():
             new_dict.append(obj.to_dict())
-            # for key, obj in type(self).__objects.items():
-            #    new_dict[key] = obj.to_dict()
+
         with open(type(self).__file_path, "w", encoding='utf-8') as file:
             json.dump(new_dict, file)
-            # OR
-            # with open(type(self).__file_path, "w", encoding="utf-8") as file:
-            #   json.dump([obj.to_dict() for obj in self.all().values()], file)
 
     def reload(self):
         """Deserializes the JSON file to __objects if it exists"""
         if os.path.exists(type(self).__file_path) is True:
             return
-        try:
-            with open(type(self).__file_path, "r") as file:
-                new_obj = json.load(file)
-                for key, val in new_obj.items():
-                    obj = self.class_dict[val['__class__']](**val)
-                    type(self).__objects[key] = obj
-        except Exception:
-            pass
+            try:
+                with open(type(self).__file_path, "r") as file:
+                    new_obj = json.load(file)
+                    for key, val in new_obj.items():
+                        obj = self.class_dict[val['__class__']](**val)
+                        type(self).__objects[key] = obj
+            except Exception:
+                pass
