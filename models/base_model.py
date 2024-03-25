f1897f23eeca4bbb43c7470c84ce6a58f8f8ae96
Lain0x0 <lainyamato03@gmail.com>
2024-03-25 00:45:56 +0000

    base_exe

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,19 @@ from datetime import datetime
 class BaseModel:
     """Base model class."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if (kwargs):
+            for key, value in kwargs.items():
+                if key != '_class_':
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.
+                        strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Return string representation of the instance."""
