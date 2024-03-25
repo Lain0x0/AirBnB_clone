1a13329d81a8ebb72fa0bd443b5ad6caab1a3307
Lain0x0 <lainyamato03@gmail.com>
2024-03-25 00:20:20 +0000

    base.exe

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,30 @@
 #!/usr/bin/python3
-"""."""
-import models
-from uuid import uuid4
+import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """."""
+    """Base model class."""
 
-    def __init__(self, *args, **kwargs):
-        """.
-        """
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-        if len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "created_at" or k == "updated_at":
-                    self._dict_[k] = datetime.strptime(v, tform)
-                else:
-                    self._dict_[k] = v
-        else:
-            models.storage.new(self)
+    def __init__(self):
+        """Initialize BaseModel instance."""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return string representation of the instance."""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
 
     def save(self):
-        """."""
-        self.updated_at = datetime.today()
-        models.storage.save()
+        """Update the 'updated_at' attribute with the current datetime."""
+        self.updated_at = datetime.now()
 
     def to_dict(self):
-        """.
-        """
-        rdict = self._dict_.copy()
-        rdict["created_at"] = self.created_at.isoformat()
-        rdict["updated_at"] = self.updated_at.isoformat()
-        rdict["_class"] = self.class .name_
-        return rdict
-
-    def _str_(self):
-        """."""
-        clname = self._class.name_
-        return "[{}] ({}) {}".format(clname, self.id, self._dict_)
+        """Return a dictionary representation of the instance."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
